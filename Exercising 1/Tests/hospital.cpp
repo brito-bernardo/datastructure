6baf29c31e319e3a4fedf0e13f2d88c0c6c27c07
brito-bernardo <bernardo_brito1@hotmail.com>
2023-11-13 22:13:31 +0000

    Exercise 1 part 1

---
+++ Exercising 1/Tests/hospital.cpp
@@ -0,0 +1,106 @@
+#include "hospital.h"
+
+Hospital::Hospital() { }
+
+void Hospital::addDoctor(const Doctor& d) {
+	doctors.push_back(d);
+}
+
+void Hospital::addPatient(const Patient& p) {
+    patients.push_back(p);
+}
+
+void Hospital::addOldPatient(const Patient& p) {
+    oldPatients.insert(p);
+}
+
+void Hospital::addStackConsultations(const stack<Consultation>& s) {
+    consultations.push_back(s);
+}
+
+vector<Doctor> Hospital::getDoctors() const {
+	return doctors;
+}
+
+list<Patient> Hospital::getPatients() const {
+    return patients;
+}
+
+set<Patient> Hospital::getOldPatients() const {
+    return oldPatients;
+}
+
+list<stack<Consultation>> Hospital::getConsultations() const {
+    return consultations;
+}
+
+void Hospital::setConsultations(const list<stack<Consultation>>& consults) {
+    consultations = consults;
+}
+
+
+// --------------------------------------------------------------------
+
+//TODO
+void Hospital::sortDoctors() {
+    std::sort(doctors.begin(), doctors.end(),[](const Doctor& a, const Doctor& b) {
+        if (a.getPatientsToAttend().size() == b.getPatientsToAttend().size()) {
+            return a.getID() < b.getID(); // Ordem crescente de ID para igualdade no tamanho da fila
+        }
+        return a.getPatientsToAttend().size() > b.getPatientsToAttend().size(); // Ordem decrescente pelo tamanho da fila
+    });
+}
+
+
+// TODO
+float Hospital::averageNPatients(string sp) const {
+    int totalPatients = 0;
+    int countDoctors = 0;
+
+    for (const Doctor& doc : doctors) {
+        if (doc.getSpecialty() == sp) {
+            totalPatients += doc.getPatientsToAttend().size();
+            countDoctors++;
+        }
+    }
+
+    if (countDoctors == 0) return 0.0f; // Evita divisão por zero
+
+    return totalPatients / (float)countDoctors; // Conversão implícita para float
+}
+
+
+// TODO
+int Hospital::removePatients(int minC) {
+    int removedPatients = 0;
+
+    for (auto it = patients.begin(); it != patients.end(); ) {
+        if (it->getNumConsultations() < minC) {
+            it = patients.erase(it);
+            removedPatients++;
+        } else {
+            ++it;
+        }
+    }
+
+    return removedPatients;
+}
+
+
+// TODO
+vector<Patient> Hospital::getOldPatients(int y) const {
+    vector<Patient> res;
+    return res;
+}
+
+
+// TODO
+void Hospital::processConsultation(Consultation c) {
+
+}
+
+
+// TODO
+bool Hospital::addDoctor(int id2, string sp2, int id1) {
+    return true;
+}
---
+++ Exercising 1/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/bernardobrito/Desktop/projetos-backend/datastructure/Exercising 1/lib/googletest-master
+# Build directory: /Users/bernardobrito/Desktop/projetos-backend/datastructure/Exercising 1/cmake-build-debug/lib/googletest-master
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
